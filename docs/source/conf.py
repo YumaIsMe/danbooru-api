36e20810dff8d301f38318480aa9442fe5f1c855
Yui <naominewedits@gmail.com>
2024-09-13 00:07:50 +0200

    Add Sphinx documentation source and configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,19 +1,32 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 project = 'Danbooru API library wrapper'
 copyright = '2024, Yuma'
 author = 'Yuma'
 release = '2024'
 
 # -- General configuration ---------------------------------------------------
-extensions = ['sphinx_rtd_theme']
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'python'
-
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
