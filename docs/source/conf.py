29045e98f90e2e1b6eb376b76ae84e8dfb8b872e
Yui <naominewedits@gmail.com>
2024-09-12 23:30:48 +0200

    Update Sphinx configuration to use sphinx-rtd-theme

---
+++ docs/source/conf.py
@@ -0,0 +1,19 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'Danbooru API library wrapper'
+copyright = '2024, Yuma'
+author = 'Yuma'
+release = '2024'
+
+# -- General configuration ---------------------------------------------------
+extensions = ['sphinx_rtd_theme']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'python'
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
